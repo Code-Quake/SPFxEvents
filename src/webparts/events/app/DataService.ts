38c14ca697573284526a4ae0ef586b5b32a40ca4
Joe Jorden <jljorden@comcast.net>
2017-12-29 15:56:10 -0700

    Added dataservice

--- src/webparts/events/app/DataService.ts
+++ src/webparts/events/app/DataService.ts
@@ -1,67 +1,103 @@
 import * as angular from 'angular';
 
-export interface ITodo {
+export interface IEvent {
     id: number;
     title: string;
-    done: boolean;
+    start: Date;
+    end: Date;
   }
-  
+
+  export interface IAttendee {
+    id: number;
+    fullname: string;
+    email: string;
+    eventid: number;
+  }
+
   export interface IDataService {
-    getTodos(hideFinishedTasks: boolean): angular.IPromise<ITodo[]>;
-    addTodo(todo: string): angular.IPromise<{}>;
-    deleteTodo(todo: ITodo): angular.IPromise<{}>;
-    setTodoStatus(todo: ITodo, done: boolean): angular.IPromise<{}>;
+    getEvents(): angular.IPromise<IEvent[]>;
+    addEvent(title: string, start: Date, end: Date): angular.IPromise<{}>;
+    deleteEvent(event: IEvent): angular.IPromise<{}>;
+    getAttendeeEvents(): angular.IPromise<IAttendee[]>;
+    addAttendeeEvent(fullName: string, email: string, eventId: number): angular.IPromise<{}>;
+    deleteAttendeeEvent(attendeeEvent: IAttendee): angular.IPromise<{}>;
   }
   
   export default class DataService implements IDataService {
     public static $inject: string[] = ['$q'];
   
-    private items: ITodo[] = [
+    private eventItems: IEvent[] = [
       {
         id: 1,
         title: 'Prepare demo Web Part',
-        done: true
+        start: new Date(2018, 1, 1),
+        end: new Date(2018, 1, 1),
       },
       {
         id: 2,
-        title: 'Show demo',
-        done: false
+        title: 'Company Meeting 1',
+        start: new Date(2018, 1, 2),
+        end: new Date(2018, 1, 2),
+      },
+      {
+        id: 1,
+        title: 'Company Meeting 2',
+        start: new Date(2018, 1, 3),
+        end: new Date(2018, 1, 3),
+      },
+    ];
+
+    private attendeeItems: IAttendee[] = [
+      {
+        id: 1,
+        fullname: 'Clark Kent',
+        email: 'ckent@dailyplanet.com',
+        eventid: 1,
+      },
+      {
+        id: 2,
+        fullname: 'Bruce Wayne',
+        email: 'bwayne@wayne.com',
+        eventid: 2,
       },
       {
         id: 3,
-        title: 'Share code',
-        done: false
-      }
+        fullname: 'Diana Prince',
+        email: 'dprince@themyscira.com',
+        eventid: 3,
+      },
     ];
+
     private nextId: number = 4;
   
     constructor(private $q: angular.IQService) {
     }
   
-    public getTodos(hideFinishedTasks: boolean): angular.IPromise<ITodo[]> {
-      const deferred: angular.IDeferred<ITodo[]> = this.$q.defer();
+    public getAttendeeEvents(): angular.IPromise<IAttendee[]> {
+      const deferred: angular.IDeferred<IAttendee[]> = this.$q.defer();
   
-      const todos: ITodo[] = [];
-      for (let i: number = 0; i < this.items.length; i++) {
-        if (hideFinishedTasks && this.items[i].done) {
-          continue;
-        }
+      const attendeeEvents: IAttendee[] = [];
+      for (let i: number = 0; i < this.attendeeItems.length; i++) {
+        // if (hideFinishedTasks && this.items[i].done) {
+        //   continue;
+        // }
   
-        todos.push(this.items[i]);
+        attendeeEvents.push(this.attendeeItems[i]);
       }
   
-      deferred.resolve(todos);
+      deferred.resolve(attendeeEvents);
   
       return deferred.promise;
     }
   
-    public addTodo(todo: string): angular.IPromise<{}> {
+    public addAttendeeEvent(fullName: string, email: string, eventId: number): angular.IPromise<{}> {
       const deferred: angular.IDeferred<{}> = this.$q.defer();
   
-      this.items.push({
+      this.attendeeItems.push({
         id: this.nextId++,
-        title: todo,
-        done: false
+        fullname: fullName,
+        email: email,
+        eventid: eventId
       });
   
       deferred.resolve();
@@ -69,19 +105,19 @@ export interface ITodo {
       return deferred.promise;
     }
   
-    public deleteTodo(todo: ITodo): angular.IPromise<{}> {
+    public deleteAttendeeEvent(attendeeEvent: IAttendee): angular.IPromise<{}> {
       const deferred: angular.IDeferred<{}> = this.$q.defer();
   
       let pos: number = -1;
-      for (let i: number = 0; i < this.items.length; i++) {
-        if (this.items[i].id === todo.id) {
+      for (let i: number = 0; i < this.attendeeItems.length; i++) {
+        if (this.attendeeItems[i].id === attendeeEvent.id) {
           pos = i;
           break;
         }
       }
   
       if (pos > -1) {
-        this.items.splice(pos, 1);
+        this.attendeeItems.splice(pos, 1);
         deferred.resolve();
       }
       else {
@@ -90,17 +126,57 @@ export interface ITodo {
   
       return deferred.promise;
     }
+
+    public getEvents(): angular.IPromise<IEvent[]> {
+      const deferred: angular.IDeferred<IEvent[]> = this.$q.defer();
   
-    public setTodoStatus(todo: ITodo, done: boolean): angular.IPromise<{}> {
+      const events: IEvent[] = [];
+      for (let i: number = 0; i < this.eventItems.length; i++) {
+        // if (hideFinishedTasks && this.items[i].done) {
+        //   continue;
+        // }
+  
+        events.push(this.eventItems[i]);
+      }
+  
+      deferred.resolve(events);
+  
+      return deferred.promise;
+    }
+  
+    public addEvent(title: string, start: Date, end: Date): angular.IPromise<{}> {
       const deferred: angular.IDeferred<{}> = this.$q.defer();
   
-      for (let i: number = 0; i < this.items.length; i++) {
-        if (this.items[i].id === todo.id) {
-          this.items[i].done = done;
+      this.eventItems.push({
+        id: this.nextId++,
+        title: title,
+        start: start,
+        end: end
+      });
+  
+      deferred.resolve();
+  
+      return deferred.promise;
+    }
+  
+    public deleteEvent(event: IEvent): angular.IPromise<{}> {
+      const deferred: angular.IDeferred<{}> = this.$q.defer();
+  
+      let pos: number = -1;
+      for (let i: number = 0; i < this.eventItems.length; i++) {
+        if (this.eventItems[i].id === event.id) {
+          pos = i;
+          break;
         }
       }
   
-      deferred.resolve();
+      if (pos > -1) {
+        this.eventItems.splice(pos, 1);
+        deferred.resolve();
+      }
+      else {
+        deferred.reject();
+      }
   
       return deferred.promise;
     }
