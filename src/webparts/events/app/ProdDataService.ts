5796997c6e30779f0a106205e8aa8a56265fc251
Joe Jorden <jljorden@comcast.net>
2018-01-02 15:34:48 -0700

    Added sp-pnp-js methods for getting events and attendees

--- src/webparts/events/app/ProdDataService.ts
+++ src/webparts/events/app/ProdDataService.ts
@@ -1,6 +1,8 @@
 import * as angular from 'angular';
 import { IEvent, IAttendee, IDataService } from './interfaces-module';
-import pnp from "sp-pnp-js";
+import pnp, { List, ListEnsureResult, ItemAddResult, FieldAddResult } from "sp-pnp-js";
+
+const LIST_EXISTS: string = 'List exists';
 
 export default class ProdDataService implements IDataService {
   public static $inject: string[] = ['$q'];
@@ -28,8 +30,26 @@ export default class ProdDataService implements IDataService {
     return deferred.promise;
   }
 
-  public getAttendeeEvents(showpastevents?: boolean): angular.IPromise<IAttendee[]> {
+  private getEvents(): angular.IPromise<IEvent[]> {
+    const deferred: angular.IDeferred<IEvent[]> = this.$q.defer();
+    let attendees = pnp.sp.web.lists.getByTitle('Events').select("Id", "Title", "StartDate", "EndDate", "Campus", "TotalAttendees").getAs<IEvent[]>();
+
+    const events: IEvent[] = [];
+    for (let i: number = 0; i < this.eventItems.length; i++) {
+      // if (hideFinishedTasks && this.items[i].done) {
+      //   continue;
+      // }
+
+      events.push(this.eventItems[i]);
+    }
+    deferred.resolve(events);
+
+    return deferred.promise;
+  }
+
+  private getAttendees(): angular.IPromise<IAttendee[]> {
     const deferred: angular.IDeferred<IAttendee[]> = this.$q.defer();
+    let attendees = pnp.sp.web.lists.getByTitle('Attendees').select("Id", "Title", "FullName", "Email", "EventID").getAs<IAttendee[]>();
 
     const attendeeEvents: IAttendee[] = [];
     for (let i: number = 0; i < this.attendeeItems.length; i++) {
@@ -39,13 +59,30 @@ export default class ProdDataService implements IDataService {
 
       attendeeEvents.push(this.attendeeItems[i]);
     }
-
     deferred.resolve(attendeeEvents);
 
     return deferred.promise;
   }
 
-  public addAttendeeEvent(attendeeEvent: IAttendee): angular.IPromise<{}> {
+
+  // public getAttendeeEvents(showpastevents?: boolean): angular.IPromise<IAttendee[]> {
+  //   const deferred: angular.IDeferred<IAttendee[]> = this.$q.defer();
+
+  //   const attendeeEvents: IAttendee[] = [];
+  //   for (let i: number = 0; i < this.attendeeItems.length; i++) {
+  //     // if (hideFinishedTasks && this.items[i].done) {
+  //     //   continue;
+  //     // }
+
+  //     attendeeEvents.push(this.attendeeItems[i]);
+  //   }
+
+  //   deferred.resolve(attendeeEvents);
+
+  //   return deferred.promise;
+  // }
+
+  public addAttendee(attendeeEvent: IAttendee): angular.IPromise<{}> {
     const deferred: angular.IDeferred<{}> = this.$q.defer();
 
     this.attendeeItems.push({
@@ -60,7 +97,7 @@ export default class ProdDataService implements IDataService {
     return deferred.promise;
   }
 
-  public deleteAttendeeEvent(attendeeEvent: IAttendee): angular.IPromise<{}> {
+  public deleteAttendee(attendeeEvent: IAttendee): angular.IPromise<{}> {
     const deferred: angular.IDeferred<{}> = this.$q.defer();
 
     let pos: number = -1;
@@ -82,22 +119,22 @@ export default class ProdDataService implements IDataService {
     return deferred.promise;
   }
 
-  public getEvents(showpastevents?: boolean): angular.IPromise<IEvent[]> {
-    const deferred: angular.IDeferred<IEvent[]> = this.$q.defer();
+  // public getEvents(showpastevents?: boolean): angular.IPromise<IEvent[]> {
+  //   const deferred: angular.IDeferred<IEvent[]> = this.$q.defer();
 
-    const events: IEvent[] = [];
-    for (let i: number = 0; i < this.eventItems.length; i++) {
-      // if (hideFinishedTasks && this.items[i].done) {
-      //   continue;
-      // }
+  //   const events: IEvent[] = [];
+  //   for (let i: number = 0; i < this.eventItems.length; i++) {
+  //     // if (hideFinishedTasks && this.items[i].done) {
+  //     //   continue;
+  //     // }
 
-      events.push(this.eventItems[i]);
-    }
+  //     events.push(this.eventItems[i]);
+  //   }
 
-    deferred.resolve(events);
+  //   deferred.resolve(events);
 
-    return deferred.promise;
-  }
+  //   return deferred.promise;
+  // }
 
   public addEvent(event: IEvent): angular.IPromise<{}> {
     const deferred: angular.IDeferred<{}> = this.$q.defer();
